848452e212c1ae8af528c69def7754b4921f8cdb
mgn12 <marcosnuero11@gmail.com>
2025-10-06 19:09:05 +0200

    trainingPlan added

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TrainingPlanModule } from './training-plan/training-plan.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, TrainingPlanModule],
   controllers: [AppController],
   providers: [AppService],
 })

fdd829717d60a576a9fb74b275df5d0e696426fb
mgn12 <marcosnuero11@gmail.com>
2025-10-06 19:16:14 +0200

    Added resources directory

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { TrainingPlanModule } from './training-plans/training-plan.module';
+import { UsersModule } from './resources/users/users.module';
+import { TrainingPlanModule } from './resources/training-plans/training-plan.module';
 import { WorkoutsModule } from './no-spec/workouts/workouts.module';
-import { WorkoutsModule } from './workouts/workouts.module';
-import { ActivitiesModule } from './activities/activities.module';
-import { ExercisesModule } from './exercises/exercises.module';
+import { WorkoutsModule } from './resources/workouts/workouts.module';
+import { ActivitiesModule } from './resources/activities/activities.module';
+import { ExercisesModule } from './resources/exercises/exercises.module';
 
 @Module({
   imports: [UsersModule, TrainingPlanModule, WorkoutsModule, ActivitiesModule, ExercisesModule],

9e088cf93798bbdf47ffb534e1cafc1a5df557df
mgn12 <marcosnuero11@gmail.com>
2025-10-08 17:52:31 +0200

    ORM add activity entity

--- backend/src/resources/activities/entities/activity.entity.ts
+++ backend/src/resources/activities/entities/activity.entity.ts
@@ -1,1 +1,20 @@
-export class Activity {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Activity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  sets: number;
+
+  @Column()
+  reps: number;
+
+  @Column()
+  weight: number;
+
+  @Column()
+  // Wait until i know the attributes of results to create the json
+  results: JSON;
+}
